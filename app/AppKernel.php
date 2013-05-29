1062a0c02f43d54347172ac5531da9f9ec51799f
willystadnick <willy.stadnick@gmail.com>
2013-05-29 07:32:23 -0300

    bootstrap

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -20,6 +20,7 @@ class AppKernel extends Kernel
             new JMS\DiExtraBundle\JMSDiExtraBundle($this),
             new JMS\SecurityExtraBundle\JMSSecurityExtraBundle(),
             new Rpgmr\RpgmrBundle\RpgmrBundle(),
+            new Bc\Bundle\BootstrapBundle\BcBootstrapBundle()
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
